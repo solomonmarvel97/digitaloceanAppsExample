0b2d1af8996aa8ab1af7fc0aea88c6df4d7cff76
Marvelous Akporowho <solomonmarvel@hotmail.com>
2023-02-07 22:00:00 +0100

    changed running port to 8080

--- index-copy.js
+++ index-copy.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const port = 3000
 const {v4} = require('uuid')
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get('/random', (req, res) => {
+	const response = `This is our radom uuid ${v4()}`
+	res.send(response)
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
