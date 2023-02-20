1a5ad5467d5689c0804ff6695c403da41d042e2c
Marvelous Akporowho <solomonmarvel@hotmail.com>
2023-02-20 23:36:43 +0100

    updated code base

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 8080
-const {v4} = require('uuid')
+const express = require("express");
+const app = express();
+const port = 8080;
+const { v4 } = require("uuid");
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) => res.send("Hello World!"));
 
-app.get('/random', (req, res) => {
-	const response = `This is our radom uuid ${v4()}`
-	res.send(response)
-})
+app.get("/random", (req, res) => {
+  const response = `This is our uuid ${v4()}`;
+  res.send(response);
+});
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
-
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
